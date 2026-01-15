26db0bc964f60acf8e2e379fc9f9704fc1c865ca
Ahmad Javed <ahmadjaved97@gmail.com>
2026-01-15 17:23:57 +0100

    image loader added

--- features/__init__.py
+++ features/__init__.py
@@ -3,9 +3,11 @@ This module provides efficient feature extraction with batch processing, HDF5 ca
 """
 
 from .metadata import FeatureMetadata
+from .loaders import ImageLoader
 
 __all__ = [
-    'FeatureMetadata'
+    'FeatureMetadata',
+    'ImageLoader',
 ]
 
 __version__ = "1.0.0"
