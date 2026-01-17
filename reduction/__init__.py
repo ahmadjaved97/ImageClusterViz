9a8236e52b746c41a216eb6f65e4da84acbe2f1d
Ahmad Javed <ahmadjaved97@gmail.com>
2026-01-17 18:38:13 +0100

    Feature reduction module and methods

---
+++ reduction/__init__.py
@@ -0,0 +1,13 @@
+"""
+Dimensionality reduction module.
+"""
+
+
+from .base import DimensionalityReducer, ReductionResult
+
+_all__ = [
+    'DimensionalityReducer',
+    'ReductionResult'
+]
+
+__version__ = '1.0.0'
